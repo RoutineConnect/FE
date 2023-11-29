6710697a59b84615090b76df7083d069e7397618
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2023-11-29 13:16:39 +0900

    feat: 기존 로그아웃버튼 삭제

--- components/layout/sideBar_R.tsx
+++ components/layout/sideBar_R.tsx
@@ -1,18 +1,3 @@
-"use client";
-
-import { LoginValue } from "@/atom";
-import { useSetRecoilState } from "recoil";
-import { deleteCookie } from "../utils/setCookie";
-
 export default function SideBar_R() {
-  const setLoginVar = useSetRecoilState(LoginValue);
-  const logOut = () => {
-    deleteCookie();
-    setLoginVar(false);
-  };
-  return (
-    <div className=" w-sideBar">
-      <div onClick={logOut}>로그아웃</div>
-    </div>
-  );
+  return <div></div>;
 }
