e67cd4919b28d2c106a3963f59cd7443713d2a0a
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-03-14 19:01:24 +0900

    feat: root page 기본 셋팅
    root page의 기본적인 ui를 만들었습니다.

---
+++ icon/logo/iLogo.tsx
@@ -0,0 +1,18 @@
+export default function ILogo() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 171 23">
+      <text
+        id="ROUTINE_CONNECT"
+        data-name="ROUTINE CONNECT"
+        transform="translate(0 18)"
+        fill="#294694"
+        fontSize="20"
+        fontFamily="Bauhaus93, 'Bauhaus \39 3'"
+      >
+        <tspan x="0" y="0">
+          ROUTINE CONNECT
+        </tspan>
+      </text>
+    </svg>
+  );
+}
